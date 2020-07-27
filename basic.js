a5a078cd2ce8c9bf44f3022b3f8aeefaf9d75c01
dazzel3 <aakk9350@naver.com>
2020-07-28 02:39:22 +0900

    [기초] 변수와 상수 선언, 여러가지 연산자

---
+++ basic.js
@@ -0,0 +1,34 @@
+//변수 
+let a = 1;
+a =2;
+
+//상수
+const b = 1;
+
+//산술연산자
+const sum = a+b;
+
+console.log(a++);
+console.log(++b);
+
+//비교연산자
+const equals = a === b;
+const notEquals = a !== b;
+
+//논리연산자
+const value = true;
+const value2 = false;
+
+//NOT !
+console.log(!value);
+
+//AND &&
+console.log(value && value2);
+
+//OR ||
+console.log(value || value2);
+
+//문자열 붙이기
+const str1 = "감사";
+const str2 = "해요";
+console.log(str1 + str2);
