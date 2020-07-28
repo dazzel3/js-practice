ed1ed9cf450a83d43ed15e5149ae602e0aeddb65
dazzel3 <aakk9350@naver.com>
2020-07-28 23:10:34 +0900

    [배열] 배열 선언 및 구현

---
+++ array.js
@@ -0,0 +1,13 @@
+const array = [1,2,3,4,5];
+console.log(array);
+console.log(array[4]);
+
+const objects = [{name: '멍멍이'},{name: '야옹이'}];
+console.log(objects[1]);
+
+objects.push({
+  name: '악어'
+});
+
+console.log(objects);
+console.log(objects.length);
